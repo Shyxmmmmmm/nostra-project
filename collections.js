191eb5fe6ae1d9dede7a2d4e1c9cea7aad54f314
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-03-14 15:40:53 +0530

    clean tailwind classes

--- collections.js
+++ collections.js
@@ -3,42 +3,52 @@ var b = document.getElementById("two")
 var c = document.getElementById("three")
 var d = document.getElementById("four")
 var e = document.getElementById("five")
+var f=document.querySelectorAll(".six")
+var g=document.querySelectorAll(".seven")
 
 
-b.addEventListener('click', function () {
-    a.style.display = 'none'
+
+
+
+b.addEventListener("click",function(){
+    a.style.display='none'
 })
 
-c.addEventListener("click", function () {
+c.addEventListener("click",function(){
     d.classList.toggle("-translate-x-full")
 })
 
-e.addEventListener('click', function () {
+e.addEventListener("click",function(){
     d.classList.add("-translate-x-full")
 })
 
-f.forEach(function (g) {
+f.forEach(function(box){
+    box.addEventListener("change",function(){
+        var checked=[]
 
-    g.addEventListener("click", function () {
+        f.forEach(function(box){
+            if (box.checked){
+                checked.push(box.value)
+            }
+        })
 
-        if (g.classList.contains("fa-solid")) {
-            g.classList.remove("fa-solid")
-            g.classList.add("fa-regular")
-            g.style.color = "rgb(22,22,22)"
-        }
-        else {
-            g.classList.add("fa-solid")
-            g.classList.remove("fa-regular")
-            g.style.color = "rgb(229,57,28)"
-        }
+        g.forEach(function(box){
+            var a1=box.dataset.occasion
+            var a2=box.dataset.color
+            var a3=box.dataset.arrivals
+
+            if(
+                checked.length===0 ||
+                checked.includes(a1) ||
+                checked.includes(a2) ||
+                checked.includes(a3)
+            ){
+                box.style.display='block'
+            }
+            else{
+                box.style.display='none'
+            }
+        })
 
     })
-
 })
-
-
-
-
-window.addEventListener("load", () => {
-    document.body.classList.remove("opacity-0");
-});
